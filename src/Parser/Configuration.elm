4010bdfbfe8937be93dc1c97fd8f80b32d9c3c9e
James Carlson <jxxcarlson@gmail.com>
2021-07-10 13:54:10 -0400

    Add markPosition field data to Configuration.expectations

--- src/Parser/Configuration.elm
+++ src/Parser/Configuration.elm
@@ -1,12 +1,13 @@
 module Parser.Configuration exposing (expectations)
 
-import Parser.Config exposing (EType(..))
+import Parser.Config exposing (EType(..), MarkPosition(..))
 
 
 expectations =
-    [ { begin = '[', end = Just ']', etype = ElementType, isVerbatim = False }
-    , { begin = '`', end = Just '`', etype = CodeType, isVerbatim = True }
-    , { begin = '$', end = Just '$', etype = InlineMathType, isVerbatim = True }
-    , { begin = '#', end = Nothing, etype = ElementType, isVerbatim = False }
-    , { begin = '"', end = Just '"', etype = QuotedType, isVerbatim = False }
+    [ { begin = '[', end = Just ']', etype = ElementType, isVerbatim = False, markPosition = Anywhere }
+    , { begin = '`', end = Just '`', etype = CodeType, isVerbatim = True, markPosition = Anywhere }
+    , { begin = '$', end = Just '$', etype = InlineMathType, isVerbatim = True, markPosition = Anywhere }
+    , { begin = '#', end = Nothing, etype = ElementType, isVerbatim = False, markPosition = AtBeginning }
+    , { begin = ':', end = Nothing, etype = ElementType, isVerbatim = False, markPosition = AtBeginning }
+    , { begin = '"', end = Just '"', etype = QuotedType, isVerbatim = False, markPosition = Anywhere }
     ]
